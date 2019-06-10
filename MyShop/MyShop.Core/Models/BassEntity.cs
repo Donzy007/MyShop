6ade927b41125ae4f54fc98fb192857f8b4552d6
Tamie Stephen <donstephco@yahoo.com>
2019-06-10 22:02:53 +0100

    implementing Generic

---
+++ MyShop/MyShop.Core/Models/BassEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public abstract class BassEntity
+    {
+        public string Id { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public BassEntity()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.CreatedAt = DateTime.Now;
+        }
+    }
+}
